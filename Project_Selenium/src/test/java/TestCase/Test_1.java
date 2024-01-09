d8e3544bc6fd6dbe886bb350eb96b4b3dcb664c3
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2024-01-09 11:12:59 +0530

    DONE

--- Project_Selenium/src/test/java/TestCase/Test_1.java
+++ Project_Selenium/src/test/java/TestCase/Test_1.java
@@ -2,12 +2,13 @@ package TestCase;
 
 import java.util.Scanner;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Test_1 {
+public class Test_1{
 	
 	public static String browser;
 	public static WebDriver driver;
@@ -17,6 +18,7 @@ public class Test_1 {
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Enter the browser name: ");
 		browser=sc.nextLine();
+		sc.close();
 		if(browser.equals("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
@@ -25,9 +27,12 @@ public class Test_1 {
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
 		}
-			driver.manage().window().maximize();
-			driver.get("https://www.google.com");
+			driver.manage().window().maximize();   //Helps to maximize the full screen for the browser 
+			driver.get("https://www.flipkart.com/");
 			System.out.println("Title of the page is: "+driver.getTitle());
+			driver.findElement(By.xpath("//span[text()='Login']")).click();
+			driver.findElement(By.xpath("//input[@class='_2IX_2- VJZDxU']")).sendKeys("8369187184");
+			driver.findElement(By.xpath("//button[text()=\"Request OTP\"]")).click();
 			driver.close();
 
 	}
