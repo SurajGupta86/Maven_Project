1f8d677c0b6dc9ed895401d7c1a8117f2e0f5baf
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2024-01-09 22:21:01 +0530

    DONE

--- Project_Selenium/src/test/java/TestCase/Test_1.java
+++ Project_Selenium/src/test/java/TestCase/Test_1.java
@@ -1,5 +1,6 @@
 package TestCase;
 
+import java.time.Duration;
 import java.util.Scanner;
 
 import org.openqa.selenium.By;
@@ -13,7 +14,7 @@ public class Test_1{
 	public static String browser;
 	public static WebDriver driver;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		// TODO Auto-generated method stub
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Enter the browser name: ");
@@ -27,13 +28,17 @@ public class Test_1{
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
 		}
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.manage().window().maximize();   //Helps to maximize the full screen for the browser 
 			driver.get("https://www.flipkart.com/");
 			System.out.println("Title of the page is: "+driver.getTitle());
 			driver.findElement(By.xpath("//span[text()='Login']")).click();
-			driver.findElement(By.xpath("//input[@class='_2IX_2- VJZDxU']")).sendKeys("8369187184");
-			driver.findElement(By.xpath("//button[text()=\"Request OTP\"]")).click();
-			driver.close();
+			//Thread.sleep(5000);
+			driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[3]/div/div[2]/div/form/div[1]/input")).sendKeys("8369187184");
+			//Thread.sleep(5000);
+			driver.findElement(By.xpath("//button[text()='Request OTP']")).click();
+			Thread.sleep(5000);
+			//driver.close();
 
 	}
 //wTGAwaTUST
