2bc4c717d606ffc427d18875b8c44605a90f5177
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2024-01-12 22:17:15 +0530

    DONE

--- Project_Selenium/src/test/java/TestCase/ScreenshotTest.java
+++ Project_Selenium/src/test/java/TestCase/ScreenshotTest.java
@@ -1,19 +1,22 @@
 package TestCase;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.Scanner;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Test_1{
-	
+public class ScreenshotTest {
 	public static String browser;
 	public static WebDriver driver;
 	
@@ -31,27 +34,24 @@ public class Test_1{
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
 		}
-
+		    //double d = Double.parseDouble("6"); 
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); //Implicit Wait
+			
 			WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10)); //Explicit wait
 			
 			driver.manage().window().maximize();   //Helps to maximize the full screen for the browser 
 			
 			driver.get("https://www.flipkart.com/");
-			System.out.println("Title of the page is: "+driver.getTitle());
 			
-			driver.findElement(By.xpath("//span[text()='Login']")).click();
-			//Thread.sleep(5000);
-			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@class=\"_2IX_2- VJZDxU\"]"))).sendKeys("8369187184");
-			//driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[3]/div/div[2]/div/form/div[1]/input")).sendKeys("8369187184");
-			//Thread.sleep(5000);
-			driver.findElement(By.xpath("//button[text()='Request OTP']")).click();
-			Thread.sleep(5000);
-			driver.findElement(By.xpath("//input[@placeholder='Search for products, brands and more']")).sendKeys("Wallet");
-			driver.findElement(By.xpath("//button[@type='submit']")).click();
-			//driver.findElements(null)
-			//driver.close();
-
+			//Capturing the ss of entire page
+			File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File("./screenshots/fullpage.png"));
+			
+			System.out.println("Title of the page is: "+driver.getTitle());
+			WebElement login=driver.findElement(By.xpath("//span[text()='Login']"));
+			
+			File screenshot1 = login.getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot1, new File("./screenshots/loginbutton.png"));
+			
 	}
-//wTGAwaTUST
 }
