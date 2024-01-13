9e2cb45c6cc074a5466138ed195c85067d0fc834
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2024-01-13 22:03:13 +0530

    DONE

---
+++ Project_Selenium/src/test/java/assignment/DemoQA.java
@@ -0,0 +1,46 @@
+package assignment;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoQA {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Suraj Gupta\\Downloads\\Drivers\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.get("https://demoqa.com/");
+		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.xpath("//h5[text()='Elements']")).click();
+		driver.findElement(By.xpath("//span[text()='Text Box']")).click();
+		
+		//Fill the Text Box page
+		driver.findElement(By.id("userName")).sendKeys("Hello");
+		driver.findElement(By.id("userEmail")).sendKeys("ABC@gmail.com");
+		driver.findElement(By.id("currentAddress")).sendKeys("XYZ Lane");
+		driver.findElement(By.id("permanentAddress")).sendKeys(" ANZ Road");
+		driver.findElement(By.id("submit")).click();
+		
+		//Take screenshot of the whole page
+		File ss = driver.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(ss, new File("./screenshots/demoqapage.jpg"));
+		
+		System.out.println("Screenshot of the page is captured and stored in a file");
+		
+		driver.close();
+		
+	}
+
+}
