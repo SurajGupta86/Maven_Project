018d5363a029bc28360866d66c100971b4527d11
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2024-01-26 22:13:52 +0530

    DONE

--- Project_Selenium/src/test/java/TestCase/tabsAndwindows.java
+++ Project_Selenium/src/test/java/TestCase/tabsAndwindows.java
@@ -1,13 +1,16 @@
 package TestCase;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class tabsAndwindows {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		
@@ -15,16 +18,31 @@ public class tabsAndwindows {
 		String title = driver.getTitle();
 		System.out.println("Title is "+title);
 		driver.manage().window().maximize();
-		String mainWinHandle = driver.getWindowHandle();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));  //implicit wait	
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //explicit wait
+		
+		String mainWinHandle = driver.getWindowHandle();  //Get the ID of a particular window
+		System.out.println("1st Tab: "+mainWinHandle);
 		
 		driver.findElement(By.id("tabButton")).click();
 		
-		driver.switchTo().window(mainWinHandle);
-		driver.findElement(By.xpath("//a[@href='https://demoqa.com']"));
 		
-//		
-//		System.out.println(driver.findElement(By.id("sampleHeading")).getText());
-//		System.out.println("Title is "+title);
+		driver.getWindowHandles().forEach(tab->driver.switchTo().window(tab));
+		
+		System.out.println("2nd Tab: "+driver.getWindowHandle());
+		Thread.sleep(5000);
+		//System.out.println("Title is "+driver.getTitle());
+		System.out.println(driver.findElement(By.id("sampleHeading")).getText());
+		
+		driver.switchTo().window(mainWinHandle);
+		driver.findElement(By.xpath("//a[@href='https://demoqa.com']")).click();
+				
+		//System.out.println(driver.findElement(By.id("sampleHeading")).getText());
+		
+		
+		driver.quit();   //Close all the tabs & windows
+		
+		//driver.close();  //Close the current tab or window
 		
 	}
 }
