340ed0b7e789ae76032d17869f67ad835d2b6a3d
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2024-01-27 11:56:35 +0530

    DONE

--- Project_Selenium/src/test/java/TestCase/tabsAndwindows.java
+++ Project_Selenium/src/test/java/TestCase/tabsAndwindows.java
@@ -1,8 +1,13 @@
 package TestCase;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,7 +15,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class tabsAndwindows {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		
@@ -24,6 +29,8 @@ public class tabsAndwindows {
 		String mainWinHandle = driver.getWindowHandle();  //Get the ID of a particular window
 		System.out.println("1st Tab: "+mainWinHandle);
 		
+		
+		//Process for the 'New Tab' button clicked
 		driver.findElement(By.id("tabButton")).click();
 		
 		
@@ -31,14 +38,28 @@ public class tabsAndwindows {
 		
 		System.out.println("2nd Tab: "+driver.getWindowHandle());
 		Thread.sleep(5000);
+		
+		//Screenshot of the page(2nd tab)
+		File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshot, new File("./screenshots/tabsscreenshot.png"));
+		
 		//System.out.println("Title is "+driver.getTitle());
 		System.out.println(driver.findElement(By.id("sampleHeading")).getText());
 		
 		driver.switchTo().window(mainWinHandle);
-		driver.findElement(By.xpath("//a[@href='https://demoqa.com']")).click();
+		//driver.findElement(By.xpath("//a[@href='https://demoqa.com']")).click();
 				
-		//System.out.println(driver.findElement(By.id("sampleHeading")).getText());
 		
+		//Process for the 'New Window' button clicked
+		driver.findElement(By.id("windowButton")).click();
+		driver.getWindowHandles().forEach(tab->driver.switchTo().window(tab));
+		driver.manage().window().maximize();
+		System.out.println("2nd window: "+driver.getWindowHandle());
+		
+		//Screenshot of the page(2nd window)
+		File screenshots = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshots, new File("./screenshots/windowsscreenshot.png"));
+		Thread.sleep(5000);
 		
 		driver.quit();   //Close all the tabs & windows
 		
