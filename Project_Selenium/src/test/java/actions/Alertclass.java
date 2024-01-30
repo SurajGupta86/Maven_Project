78abd10eac0bde191b3bf64ef37f04ab5e041a99
Suraj Gupta <Suraj Gupta@DESKTOP-LN0E96G>
2024-01-30 11:51:07 +0530

    DONE

--- Project_Selenium/src/test/java/actions/Alertclass.java
+++ Project_Selenium/src/test/java/actions/Alertclass.java
@@ -1,20 +1,26 @@
 package actions;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Alertclass {
 	
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
 
 		WebDriverManager.chromedriver().setup();
@@ -27,16 +33,29 @@ public class Alertclass {
 		String title = driver.getTitle();
 		System.out.println("Title is "+title);
 		
+		//Perform the Alert function for getting an alert window
 //		driver.findElement(By.id("alertButton")).click();
 //		Alert alt = driver.switchTo().alert();
 //		System.out.println(alt.getText());
 		
-		driver.findElement(By.id("timerAlertButton")).click();
-		Alert alt = driver.switchTo().alert();
-		System.out.println(alt.getText());
-		alt.wait();
+		//Perform the Alert function for getting an alert window after 5 seconds
+//		driver.findElement(By.id("timerAlertButton")).click();
+//		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));  //Using explicit wait to look for alert being present or not
+//		wait.until(ExpectedConditions.alertIsPresent());
+//		Alert alt_1 = driver.switchTo().alert();
+//		System.out.println(alt_1.getText());
+//		alt_1.accept();
 		
-		alt.accept();
+		//Perform the Alert function for getting an alert window with 'Ok' and 'Cancel' option
+		driver.findElement(By.id("confirmButton")).click();
+		File screenshot = driver.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(screenshot, new File("./screenshots/alert.png"));
+		Alert alt1 = driver.switchTo().alert();
+		Thread.sleep(5000);
+		System.out.println(alt1.getText());
+		alt1.dismiss();
+		
+		
 		
 	}
 }
